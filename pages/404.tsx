bfa465786d94b21499b814dce9bb133efd1ff54c
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2022-05-27 21:49:41 +0430

    FEAT: Add the 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const NotFound: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>404 | Margelo</title>
+      </Head>
+      Not Found
+    </>
+  );
+};
+
+export default NotFound;
