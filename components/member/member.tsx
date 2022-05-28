708b076ad9184130c627520a03a06f2ef376c5a9
amirhossein khakbaz <danialkhakbaz.dk@gmail.com>
2022-05-28 12:34:38 +0430

    FIX: Change the size of the Member component's images and Add the Babel

--- components/member/member.tsx
+++ components/member/member.tsx
@@ -10,7 +10,7 @@ const Member: React.FC<MemberProps> = ({ name, image, socialId }) => {
   return (
     <>
       <div>
-        <Image src={image} width={1266} height={1455} alt={name} />
+        <Image src={image} width={1200} height={1400} alt={name} />
         <div>
           <h3 className="text-2xl xl:text-3xl">{name}</h3>
           <h4 className="text-base">{socialId}</h4>
