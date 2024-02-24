a6f8bd2ac99de69c17c435e0e5f651c46fd8a0c8
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-24 17:46:56 +0330

    (seo): Fix the links of the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,13 @@ export const metadata: Metadata = {
     "twitter:url": "https://software-agency-website.vercel.app/",
     "twitter:domain": "software-agency-website.vercel.app",
     "twitter:image":
-      "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/software-agency-website.vercel.app/Margelo%20Software%20Agency%20Webiste/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F31e1fc05-2c1f-4d04-92b8-f77cdaea8c21.jpg%3Ftoken%3Dd7t0O-DhIu9_R1kAuZuvj1TYKXz6OfWOuxhQlbeICwA%26height%3D750%26width%3D1200%26expires%3D33244784053/og.png",
+      "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/software-agency-website.vercel.app/Margelo%20Software%20Agency%20Webiste/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F71222441-43b8-421c-8aae-79903482f117.jpg%3Ftoken%3DZgMguuKUga-2aLUMYGkbGQwDbdvoha_MJo-CmtsXKzU%26height%3D675%26width%3D1200%26expires%3D33244784186/og.png",
     "twitter:card": "summary_large_image",
     "og:title": "Margelo Software Agency Webiste",
     "og:description": "Welcome to Margelo Software Agency Website.",
     "og:url": "https://software-agency-website.vercel.app/",
-    "og:image": "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/software-agency-website.vercel.app/Margelo%20Software%20Agency%20Webiste/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F31e1fc05-2c1f-4d04-92b8-f77cdaea8c21.jpg%3Ftoken%3Dd7t0O-DhIu9_R1kAuZuvj1TYKXz6OfWOuxhQlbeICwA%26height%3D750%26width%3D1200%26expires%3D33244784053/og.png",
+    "og:image":
+      "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/software-agency-website.vercel.app/Margelo%20Software%20Agency%20Webiste/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F71222441-43b8-421c-8aae-79903482f117.jpg%3Ftoken%3DZgMguuKUga-2aLUMYGkbGQwDbdvoha_MJo-CmtsXKzU%26height%3D675%26width%3D1200%26expires%3D33244784186/og.png",
     "og:type": "website",
   },
 };
